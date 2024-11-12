d986e597d9c48ca20f7307d08608186725b4a400
yunagosh <yunagosh7@gmail.com>
2024-11-12 12:36:43 -0300

    clean repo

--- config/app.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-    const APP_URL="http://localhost/proyecto_analisis/";
-    const APP_NAME="analisis uader";
-    const APP_SESSION_NAME="uader";
-    date_default_timezone_set("America/Argentina/Buenos_Aires");
--- config/server.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-    const DB_SERVER="localhost";
-    const DB_NAME="sistema_analisis";
-    const DB_USER="root";
-    const DB_PASS="";
